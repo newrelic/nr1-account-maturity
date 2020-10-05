238045daccb9cdb129100702dd62a47036c9386b
kpeet <kpeet@newrelic.com>
2020-10-05 18:03:02 -0400

    tag filter

--- nerdlets/maturity-nerdlet/index.js
+++ nerdlets/maturity-nerdlet/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NerdGraphQuery } from 'nr1';
+import { Dropdown, DropdownSection, DropdownItem, NerdGraphQuery } from 'nr1';
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import {
@@ -21,10 +21,129 @@ import {
 export default class MaturityApplication extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      allTags: null,
+      searchText: '',
+      selectedTag: null
+    }
+  }
+
+  async componentDidMount() {
+    await this.constructTags();
+  }
+
+  async constructTags(){
+    let domains = ['APM', 'INFRA', 'SYNTH']; //start with these domains for sample set
+    let allTags = {};
+    let proms = [];
+
+    for (let domain of domains) {
+      let q = `
+      {
+        actor {
+          entitySearch(queryBuilder: {domain: ${domain}}) {
+            results {
+              entities {
+                tags {
+                  key
+                  values
+                }
+              }
+            }
+          }
+        }
+      }
+      `
+      proms.push(this.queryTags(q));
+    }
+
+    Promise.all(proms).then(resp => {
+      let allTags = [];
+      let flattend = resp.flat();
+      let first = true;
+
+      let i = 0;
+      console.log("flat")
+      console.log(flattend);
+      for (let entityTags of flattend) {
+        if (first == true) {
+          for (let aTag of entityTags.tags) {
+            allTags.push({title: aTag.key, items: []});
+            allTags[i].items.push(aTag.values[0]);
+            i++;
+          }
+          first = false;
+        } else {
+          for (let anotherTag of entityTags.tags) {
+            let obj = allTags.filter(o => {
+              return o.title == anotherTag.key;
+            })
+            if (obj.length > 0) {
+              if (obj[0].items.includes(anotherTag.values[0])) {
+                //do nothing
+              } else {
+                obj[0].items.push(anotherTag.values[0]);
+              }
+            } else {
+              allTags.push({title: anotherTag.key, items: []});
+              allTags[i].items.push(anotherTag.values[0]);
+            }
+          }
+        }
+      }
+
+      this.setState({allTags: allTags});
+    })
+  }
+
+  async queryTags(q) {
+    let res = await NerdGraphQuery.query({query: q});
+    if (res.errors) {
+      console.debug(res);
+    } else {
+      let aSetOfTags = res.data.actor.entitySearch.results.entities;
+      return aSetOfTags;
+    }
+  }
+
+  selected = (tagVal) => {
+    const { allTags } = this.state;
+    let tagKey = null;
+
+    for (let tag of allTags) {
+      if (tag.items.includes(tagVal)) {
+        tagKey = tag.title;
+      }
+    }
+
+    this.setState({ selectedTag: tagKey + ':' + tagVal });
+  }
+
+  renderTagFilter() {
+    const { allTags, searchText } = this.state;
+
+    if (allTags == null) {
+      return ''
+    } else {
+      return (
+        <Dropdown style={{float: 'right'}} title='Tags' items={allTags} sectioned search={searchText} onSearch={this.handleSearch}>
+        {({ item: section, index }) => (
+          <DropdownSection key={index} title={section.title} items={section.items}>
+          {({ item, index }) => (
+            <DropdownItem onClick={() => this.selected(item)}>{item}</DropdownItem>
+          )}
+          </DropdownSection>
+        )}
+        </Dropdown>
+      )
+    }
   }
 
   render() {
+    console.log(this.state);
     return (
+      <>
+      {this.renderTagFilter()}
       <Tabs forceRenderTabPanel>
         <TabList>
           <Tab>Overview</Tab>
@@ -38,7 +157,7 @@ export default class MaturityApplication extends React.Component {
           <Tab>Programmability</Tab>
           <Tab>Mobile</Tab>
         </TabList>
-        <ApplicationCtxProvider nr1graph={NerdGraphQuery}>
+        <ApplicationCtxProvider tag={this.state.selectedTag} nr1graph={NerdGraphQuery}>
           <MaturityScoreCtxProvider>
             <TabPanel>
               <OverviewPanel />
@@ -70,6 +189,7 @@ export default class MaturityApplication extends React.Component {
           </MaturityScoreCtxProvider>
         </ApplicationCtxProvider>
       </Tabs>
+      </>
     );
   }
 }
