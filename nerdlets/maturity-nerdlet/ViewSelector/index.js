39495b30f35b95e174e373c4095da73eae2a771b
Kav91 <kav.pather@gmail.com>
2023-12-19 03:59:50 +0000

    feat: view selector upgrade

--- nerdlets/maturity-nerdlet/ViewSelector/index.js
+++ nerdlets/maturity-nerdlet/ViewSelector/index.js
@@ -10,12 +10,14 @@ export default function ViewSelector() {
     setDataState,
     unsavedRun,
     viewConfigs,
+    viewHistory,
     loadHistoricalResult,
     selectedAccountId,
     selectedReport,
     setDefaultView,
     userSettings,
     runView,
+    toggleFavoriteView,
   } = useContext(DataContext);
   const [viewSearch, setViewSearch] = useState('');
   const favorites = userSettings?.favorites || [];
@@ -29,8 +31,6 @@ export default function ViewSelector() {
       }));
 
     const items = [...configs];
-
-    // const items = [{ id: 'allData', name: 'All data' }, ...configs];
 
     const filteredItems = items.filter(({ name }) =>
       (name || '').toLowerCase().includes((viewSearch || '').toLowerCase())
@@ -53,58 +53,108 @@ export default function ViewSelector() {
               .sort(
                 (a, b) => favorites.includes(b.id) - favorites.includes(a.id)
               )
-              .map(item => (
-                <DropdownItem
-                  onClick={() => {
-                    if (item.id === 'allData') {
+              .map(item => {
+                const viewConfig = viewConfigs.find(vc => vc.id === item.id);
+                const latestHistory = viewConfig?.history?.[0];
+
+                const onClick = () => {
+                  // if (item.id === 'allData') {
+                  //   runView(
+                  //     { id: 'allData', name: 'All data' },
+                  //     null,
+                  //     false,
+                  //     true
+                  //   );
+                  // } else
+
+                  if (
+                    item.id !== selectedView.id
+                    //  && item.id !== 'allData'
+                  ) {
+                    if (latestHistory) {
+                      console.log('load previous', viewConfig);
+                      loadHistoricalResult(viewConfig, latestHistory);
+                    } else if (viewConfig) {
+                      console.log('fresh run', viewConfig);
                       runView(
-                        { id: 'allData', name: 'All data' },
-                        null,
+                        {
+                          name: viewConfig.document.name,
+                          account: selectedAccountId,
+                        },
+                        { ...viewConfig },
                         false,
                         true
                       );
-                    } else if (
-                      item.id !== selectedView.id &&
-                      item.id !== 'allData'
-                    ) {
-                      const viewConfig = viewConfigs.find(
-                        vc => vc.id === item.id
-                      );
-
-                      const previousResult = viewConfig?.history?.[0];
-                      if (previousResult) {
-                        console.log('load previous', viewConfig);
-                        loadHistoricalResult(viewConfig, previousResult);
-                      } else if (viewConfig) {
-                        console.log('fresh run', viewConfig);
-                        runView(
-                          {
-                            name: viewConfig.document.name,
-                            account: selectedAccountId,
-                          },
-                          { ...viewConfig },
-                          false,
-                          true
-                        );
-                      }
                     }
-                  }}
-                  key={item.id}
-                  style={{
-                    fontWeight: selectedView?.id === item.id ? 'bold' : 'none',
-                  }}
-                >
-                  {favorites.includes(item.id) && (
-                    <Icon
-                      color="#F0B400"
-                      style={{ marginTop: '-3px', paddingRight: '3px' }}
-                      type={Icon.TYPE.PROFILES__EVENTS__FAVORITE__WEIGHT_BOLD}
-                    />
-                  )}
+                  }
+                };
 
-                  {item.name}
-                </DropdownItem>
-              ))}
+                return (
+                  <DropdownItem
+                    key={item.id}
+                    style={{
+                      fontWeight:
+                        selectedView?.id === item.id ? 'bold' : 'none',
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      <div
+                        style={{ flex: -1 }}
+                        onClick={e => {
+                          e.preventDefault();
+                          toggleFavoriteView(item.id);
+                        }}
+                      >
+                        {favorites.includes(item.id) ? (
+                          <Icon
+                            color="#F0B400"
+                            style={{ marginTop: '-3px', paddingRight: '3px' }}
+                            type={
+                              Icon.TYPE.PROFILES__EVENTS__FAVORITE__WEIGHT_BOLD
+                            }
+                          />
+                        ) : (
+                          <Icon
+                            style={{ marginTop: '-3px', paddingRight: '3px' }}
+                            type={Icon.TYPE.PROFILES__EVENTS__FAVORITE}
+                          />
+                        )}
+                      </div>
+
+                      <div style={{ flex: 1 }} onClick={onClick}>
+                        {item.name}
+                        {userSettings?.defaultViewId === item.id ? (
+                          <span style={{ fontStyle: 'italic' }}>
+                            &nbsp;(default)
+                          </span>
+                        ) : (
+                          ''
+                        )}
+                      </div>
+
+                      <div
+                        style={{
+                          textAlign: 'right',
+                          fontWeight: 'lighter',
+                          cursor: 'text',
+                        }}
+                      >
+                        &nbsp;&nbsp;&nbsp;
+                        {latestHistory?.document?.runAt
+                          ? new Date(
+                              latestHistory?.document?.runAt
+                            ).toLocaleString()
+                          : ''}
+                      </div>
+                    </div>
+                  </DropdownItem>
+                );
+              })}
           </DropdownSection>
 
           <DropdownSection title="">
@@ -203,5 +253,12 @@ export default function ViewSelector() {
         </Dropdown>
       </div>
     );
-  }, [view, viewSearch, selectedView, unsavedRun, viewConfigs]);
+  }, [
+    view,
+    viewSearch,
+    selectedView,
+    unsavedRun,
+    viewConfigs,
+    toggleFavoriteView,
+  ]);
 }
